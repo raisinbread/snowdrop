83c406f7c3bcd5874b1743b47a904d577e104413
John Anderson <anderson.johnd@gmail.com>
2013-02-26 19:54:45 -0700

    Initial commit.

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a README file that has not been written yet.
---
+++ snowdrop.js
@@ -0,0 +1,29 @@
+#!/usr/bin/env node
+
+var path   = require('path')
+, fs       = require('fs')
+, clc      = require('cli-color')
+, async    = require('async')
+;
+
+var markerFileName = '.snowdrop.json'
+var configFile     = null;
+
+var cwd = process.cwd();
+var configFilePaths = [cwd + path.sep + markerFileName];
+while(cwd != path.sep) {
+  configFilePaths.push(path.dirname(cwd) + path.sep + markerFileName);
+  cwd = path.dirname(cwd);
+}
+
+async.filter(configFilePaths, fs.exists, function(results){
+  if(results.length > 0) {
+    configFile = results.pop();
+    // start watching, based on config
+  } else {
+    console.log(clc.red('No config file exists.'));
+    console.log(clc.yellow("Initializing config at " + process.cwd() + path.sep + markerFileName));
+    require('./lib/create').create(markerFileName);
+  }
+});
+

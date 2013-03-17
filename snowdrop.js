4474eb16f10698b22b25822f55fb8ca5e0169cc1
John Anderson <anderson.johnd@gmail.com>
2013-03-16 22:02:46 -0600

    Refactoring for bettter testability.

--- snowdrop.js
+++ snowdrop.js
@@ -9,25 +9,43 @@ var path   = require('path')
 var markerFileName = '.snowdrop.json'
 var configFile     = null;
 
-var cwd = process.cwd();
-var configFilePaths = [cwd + path.sep + markerFileName];
-while(cwd != path.sep) {
-  configFilePaths.push(path.dirname(cwd) + path.sep + markerFileName);
-  cwd = path.dirname(cwd);
+module.exports = {
+  'getPaths':function(dir) {
+    var cwd = process.cwd();
+    if(typeof(dir) != 'undefined' && fs.existsSync(dir)) {
+      cwd = dir;
+    }
+    var configFilePaths = [cwd + path.sep + markerFileName];
+    while(cwd != path.sep) {
+      configFilePaths.push(path.dirname(cwd) + path.sep + markerFileName);
+      cwd = path.dirname(cwd);
+    }
+    return configFilePaths;
+  },
+  'configInPath':function(configFilePaths, callback) {
+    async.filter(configFilePaths, fs.exists, function(results){
+      if(results.length > 0) {
+        callback(true, results.pop());
+      } else {
+        callback(false);
+      }
+    });
+  }
 }
 
-async.filter(configFilePaths, fs.exists, function(results){
-  if(results.length > 0) {
-    configFile = results.pop();
-    if(process.argv.length > 2 && process.argv[2] == '-u') {
-      require('./lib/update').update(configFile);
+// Don'r run if we're running the test suite.
+if(process.argv[1].match(/mocha/) == null) {
+  module.exports.configInPath(module.exports.getPaths(), function(result, configFile){
+    if(result) {
+      if(process.argv.length > 2 && process.argv[2] == '-u') {
+        require('./lib/update').update(configFile);
+      } else {
+        require('./lib/observe').observe(markerFileName, configFile);
+      }
     } else {
-      require('./lib/observe').observe(markerFileName, configFile);
+      console.log(clc.red('No config file exists.'));
+      console.log(clc.yellow("Initializing config at " + process.cwd() + path.sep + markerFileName));
+      require('./lib/create').create(markerFileName);
     }
-  } else {
-    console.log(clc.red('No config file exists.'));
-    console.log(clc.yellow("Initializing config at " + process.cwd() + path.sep + markerFileName));
-    require('./lib/create').create(markerFileName);
-  }
-});
-
+  });
+}
